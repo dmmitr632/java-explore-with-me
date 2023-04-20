3f8e63cdc9ca952d508a22c1a6285ffc576cce21
dmmitr632 <df6tb43@yandex.ru>
2023-04-20 09:41:27 +0300

    fix: fix docker-compose.yml

--- stats-server/src/main/resources/schema.sql
+++ stats-server/src/main/resources/schema.sql
@@ -3,8 +3,8 @@ DROP TABLE IF EXISTS endpoints_hits;
 CREATE TABLE endpoints_hits
 (
     endpoint_hit_ids int GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
-    app              varchar(50)                                  NOT NULL,
-    uri              varchar(256),
-    ip               varchar(50),
+    app              varchar(512)                                  NOT NULL,
+    uri              varchar(512),
+    ip               varchar(512),
     timestamp        timestamp                                    NOT NULL
 )
