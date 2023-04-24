17add9f3a8389ed7122683172d693ebde0537ebe
dmmitr632 <df6tb43@yandex.ru>
2023-04-24 08:22:41 +0300

    fix: fix bugs

--- stats-service/stats-server/src/main/java/ru/practicum/service/EndPointHitServiceImpl.java
+++ stats-service/stats-server/src/main/java/ru/practicum/service/EndPointHitServiceImpl.java
@@ -1,7 +1,6 @@
 package ru.practicum.service;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.dto.EndpointHitDto;
