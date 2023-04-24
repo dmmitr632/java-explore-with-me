8cdd745128b5213810ad162281031f5a5b6792c1
dmmitr632 <df6tb43@yandex.ru>
2023-04-24 07:35:43 +0300

    fix: fix pom.xml

--- main-service/src/main/java/ru/practicum/MainServiceApp.java
+++ main-service/src/main/java/ru/practicum/MainServiceApp.java
@@ -1,11 +1,13 @@
 package ru.practicum;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StatsServiceApp {
+
+public class MainServiceApp {
     public static void main(String[] args) {
-        SpringApplication.run(StatsServiceApp.class, args);
+        SpringApplication.run(MainServiceApp.class, args);
     }
 }
