10e275d4a27d6537bcbf84d25f6a8a77846ef715
dmmitr632 <df6tb43@yandex.ru>
2023-04-26 03:36:32 +0300

    fix: fix refactoring by changing sql queues

--- stats-service/stats-server/src/main/java/ru/practicum/stats/statsserver/repository/EndpointHitRepository.java
+++ stats-service/stats-server/src/main/java/ru/practicum/stats/statsserver/repository/EndpointHitRepository.java
@@ -13,14 +13,14 @@ import java.util.Set;
 @Repository
 public interface EndpointHitRepository extends JpaRepository<EndpointHit, Integer> {
 
-    @Query("SELECT new ru.practicum.dto.ViewStatsDto(eh.app, eh.uri, count(distinct eh.ip)) " +
+    @Query("SELECT new ru.practicum.stats.statsdto.dto.ViewStatsDto(eh.app, eh.uri, count(distinct eh.ip)) " +
             "FROM EndpointHit eh " +
             "WHERE eh.timestamp >= ?1 AND eh.timestamp <= ?2 AND eh.uri in ?3 " +
             "GROUP BY eh.app, eh.uri, eh.ip " +
             "ORDER BY COUNT(eh) desc")
     Collection<ViewStatsDto> getStatisticUniqueIps(LocalDateTime start, LocalDateTime end, Set<String> uris);
 
-    @Query("SELECT new ru.practicum.dto.ViewStatsDto(eh.app, eh.uri, count(eh)) " +
+    @Query("SELECT new ru.practicum.stats.statsdto.dto.ViewStatsDto(eh.app, eh.uri, count(eh)) " +
             "FROM EndpointHit eh " +
             "WHERE eh.timestamp >= ?1 AND eh.timestamp <= ?2 AND eh.uri in ?3 " +
             "GROUP BY eh.app, eh.uri " +
@@ -28,14 +28,14 @@ public interface EndpointHitRepository extends JpaRepository<EndpointHit, Intege
     Collection<ViewStatsDto> getStatisticAllIps(LocalDateTime start, LocalDateTime end, Set<String> uris);
 
 
-    @Query("SELECT new ru.practicum.dto.ViewStatsDto(eh.app, eh.uri, count(distinct eh.ip)) " +
+    @Query("SELECT new ru.practicum.stats.statsdto.dto.ViewStatsDto(eh.app, eh.uri, count(distinct eh.ip)) " +
             "FROM EndpointHit eh " +
             "WHERE eh.timestamp >= ?1 AND eh.timestamp <= ?2 " +
             "GROUP BY eh.app, eh.uri, eh.ip " +
             "ORDER BY COUNT(eh) desc")
     Collection<ViewStatsDto> getAllStatisticUniqueIps(LocalDateTime start, LocalDateTime end);
 
-    @Query("SELECT new ru.practicum.dto.ViewStatsDto(eh.app, eh.uri, count(eh)) " +
+    @Query("SELECT new ru.practicum.stats.statsdto.dto.ViewStatsDto(eh.app, eh.uri, count(eh)) " +
             "FROM EndpointHit eh " +
             "WHERE eh.timestamp >= ?1 AND eh.timestamp <= ?2 " +
             "GROUP BY eh.app, eh.uri " +
