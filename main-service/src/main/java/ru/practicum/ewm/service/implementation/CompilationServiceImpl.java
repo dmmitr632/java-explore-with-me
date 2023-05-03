183846069a8955a3e1ac46102b4ddd50c61ea82c
dmmitr632 <df6tb43@yandex.ru>
2023-05-03 12:02:57 +0300

    fix: fix CompilationServiceImpl method

--- main-service/src/main/java/ru/practicum/ewm/service/implementation/CompilationServiceImpl.java
+++ main-service/src/main/java/ru/practicum/ewm/service/implementation/CompilationServiceImpl.java
@@ -11,8 +11,10 @@ import ru.practicum.ewm.dto.UpdateCompilationRequest;
 import ru.practicum.ewm.exception.NotFoundException;
 import ru.practicum.ewm.mapper.CompilationMapper;
 import ru.practicum.ewm.model.Compilation;
+import ru.practicum.ewm.model.Event;
 import ru.practicum.ewm.repository.CompilationRepository;
 import ru.practicum.ewm.repository.EventRepository;
+import ru.practicum.ewm.repository.ParticipationRequestRepository;
 import ru.practicum.ewm.service.CompilationService;
 
 import java.util.Collection;
@@ -24,12 +26,15 @@ public class CompilationServiceImpl implements CompilationService {
     private final CompilationRepository compilationRepository;
     private final EventRepository eventRepository;
 
-    public CompilationServiceImpl(CompilationRepository compilationRepository, EventRepository eventRepository) {
+
+
+    public CompilationServiceImpl(CompilationRepository compilationRepository, EventRepository eventRepository,
+                                  ParticipationRequestRepository participationRequestRepository) {
         this.compilationRepository = compilationRepository;
         this.eventRepository = eventRepository;
     }
 
-
+    @Override
     @Transactional
     public CompilationDto addCompilation(NewCompilationDto newCompilationDto) {
         Compilation compilation = CompilationMapper.toCompilation(newCompilationDto);
@@ -51,7 +56,7 @@ public class CompilationServiceImpl implements CompilationService {
     public CompilationDto editCompilation(Integer compId, UpdateCompilationRequest updateCompilationRequest) {
 
         Compilation compilation = compilationRepository.findById(compId)
-                .orElseThrow(() -> new NotFoundException("Compilation not found"));
+                .orElseThrow(() -> new NotFoundException("Компиляции не существут"));
         if (updateCompilationRequest.getPinned() != null) {
             compilation.setPinned(updateCompilationRequest.getPinned());
         }
@@ -62,12 +67,12 @@ public class CompilationServiceImpl implements CompilationService {
         return CompilationMapper.toCompilationDto(compilationRepository.save(compilation));
     }
 
-
+    @Override
     public CompilationDto getCompilation(Integer compId) {
         return CompilationMapper.toCompilationDto(compilationRepository.findById(compId).orElseThrow(()
-                -> new NotFoundException("Compilation not found")));
+                -> new NotFoundException("Компиляции не существут")));
     }
-
+    @Override
     public Collection<CompilationDto> getCompilations(Boolean pinned, Integer from, Integer size) {
         Page<Compilation> compilations;
         Pageable pageable = PageRequest.of(from, size);
@@ -79,6 +84,17 @@ public class CompilationServiceImpl implements CompilationService {
         return compilations.stream().map(CompilationMapper::toCompilationDto).collect(Collectors.toList());
     }
 
+
+    // Additional methods, work in progress
+
+    public void deleteCompilationEvent(Integer compId, Integer eventId) {
+        Compilation compilation = compilationRepository.findById(compId).orElseThrow(()
+                -> new NotFoundException("Компиляции не существут"));
+        Event event = eventRepository.findById(eventId).orElseThrow(()
+                -> new NotFoundException("События не существут"));
+        compilation.getEvents().remove(event);
+        compilationRepository.save(compilation);
+    }
 
 }
 
