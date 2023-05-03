30f779550e2337cb95ee77a01859e9bc9ec28700
dmmitr632 <df6tb43@yandex.ru>
2023-05-03 08:28:39 +0300

    fix: fix docker-compose

--- main-service/src/main/java/ru/practicum/ewm/repository/EventRepository.java
+++ main-service/src/main/java/ru/practicum/ewm/repository/EventRepository.java
@@ -30,11 +30,11 @@ public interface EventRepository extends JpaRepository<Event, Integer> {
 //            "AND :categories IS null OR e.category.id in :categories " +
 //            "AND :start IS null OR e.eventDate >= :start " +
 //            "AND :end IS null OR e.eventDate <= :end " +
-//            "AND :states IS null OR e.eventState in :states ")
+//            "AND :states IS null OR e.state in :states ")
     @Query("SELECT e FROM Event AS e " +
             "WHERE ((:users) IS NULL OR e.initiator.id IN :users) " +
             "AND ((:categories) IS NULL OR e.category.id IN :categories)" +
-            "AND ((:states) IS NULL OR e.eventState IN :states) ")
+            "AND ((:states) IS NULL OR e.state IN :states) ")
     Page<Event> getSelectedEvents(Collection<Integer> usersIds, Collection<EventState> states,
                                   Collection<Integer> categories,
                                   LocalDateTime start, LocalDateTime end, Pageable pageable);
@@ -51,7 +51,7 @@ public interface EventRepository extends JpaRepository<Event, Integer> {
             "OR lower(e.description) LIKE %:text% " +
             "AND (:categories IS NULL OR e.category.id IN :categories) " +
             "AND (:paid IS null OR e.paid = :paid) " +
-            "AND e.eventState IN :eventState")
+            "AND e.state IN :eventState")
     Page<Event> getEvents(String text, Collection<Integer> categories, Boolean paid, LocalDateTime start,
                           LocalDateTime end, EventState eventState, Pageable pageable);
 }
